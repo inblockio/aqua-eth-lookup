33546d6fb54201af1202dbfcab8bf638148461d7
Tim Bansemer <tim.bansemer@inblock.io>
2024-06-03 10:32:29 +0200

    Add argument support for lookup function

--- eth_get_transaction_infura/infura_rs/src/main.rs
+++ eth_get_transaction_infura/infura_rs/src/main.rs
@@ -9,6 +9,10 @@ use std::convert::TryFrom;
 use dotenv::dotenv;
 use std::collections::HashMap;
 
+//adding tests
+#[cfg(test)]
+mod tests;
+
 // This clode does eth_look of the Guardian components
 // Using the infura service for Ethereum API calls https://www.infura.io/
 // This code should support ethereum sepolia, holesky and mainnet networks
@@ -31,15 +35,12 @@ fn chain_id_to_url() -> HashMap<u32, &'static str> {
     map
 }
 
-
-#[tokio::main]
-async fn main() -> Result<()> {
+async fn get_tx_data(chain_id:u32, tx_hash:&str) -> Result<()> {
 
     let my_map = chain_id_to_url(); //retrieve the HashMap
 
-    println!("Test chain_id_to_name: 0x1 should be mainnet");
-    if let Some(url_prefix) = my_map.get(&0x1) {
-        println!("The network name for chain ID 0x1 is {}", url_prefix);
+    if let Some(url_prefix) = my_map.get(&chain_id) {
+        println!("The network name for chain ID is {}", url_prefix);
     }
 
     //load .env file
@@ -48,7 +49,7 @@ async fn main() -> Result<()> {
     //Load infura API key from .env file
     let infura_api_key = std::env::var("INFURA_API_KEY").expect("INFURA_API_KEY must be set");
     //build URL
-    let url_prefix = my_map.get(&0x1).unwrap();
+    let url_prefix = my_map.get(&chain_id).unwrap();
     //println!("The network name for chain ID 0x1 is {}", url_prefix);
     
 
@@ -76,11 +77,22 @@ async fn main() -> Result<()> {
     let client = SignerMiddleware::new(provider, wallet);
 
     //Ethereum_types {H160,H256,U256} https://kauri.io/#collections/A%20Hackathon%20Survival%20Guide/sending-ethereum-transactions-with-rust/
-   let transaction_hash: H256 ="0x9d4897d3e381982ee872cb193469d991cce8d087f0cd5fe275926f80c1326a1e".parse().unwrap();
+   //let transaction_hash: H256 ="0x9d4897d3e381982ee872cb193469d991cce8d087f0cd5fe275926f80c1326a1e".parse().unwrap();
+   let transaction_hash: H256 =tx_hash.parse().unwrap();
 
    let tx = client.get_transaction(transaction_hash).await?;
 
    println!("Sent tx: {}\n", serde_json::to_string(&tx)?);
 
    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+//mainnet
+//Ok(get_tx_data(0x1,"0x9d4897d3e381982ee872cb193469d991cce8d087f0cd5fe275926f80c1326a1e").await?)
+//holesky
+//Ok(get_tx_data(0x4268,"0xe20ee33fe150423099d6c22bf84683e19d03e40371e2c76e59293d026e8d0101").await?)
+//sepolia
+Ok(get_tx_data(0xaa36a7,"0xae9b476d8eed73897b0f71ac59c267856dbae64f249518fea862377208436cc5").await?)
 }
