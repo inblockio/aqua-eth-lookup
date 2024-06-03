801823f1473eb5b70e9439facdeb80ed24806410
Tim Bansemer <tim.bansemer@inblock.io>
2024-06-03 10:10:34 +0200

    Add simple hash-map for chain_id to url mapping

--- eth_get_transaction_infura/infura_rs/src/main.rs
+++ eth_get_transaction_infura/infura_rs/src/main.rs
@@ -7,6 +7,7 @@ use ethers::{
 use eyre::Result;
 use std::convert::TryFrom;
 use dotenv::dotenv;
+use std::collections::HashMap;
 
 // This clode does eth_look of the Guardian components
 // Using the infura service for Ethereum API calls https://www.infura.io/
@@ -17,8 +18,29 @@ use dotenv::dotenv;
 //Parse the received data into serde_json
 //Provide an internal interface
 
+// Test-data inputs to verify functionality of the function
+// (mainnet, 0x9d4897d3e381982ee872cb193469d991cce8d087f0cd5fe275926f80c1326a1e)
+// (holesky, 0xe20ee33fe150423099d6c22bf84683e19d03e40371e2c76e59293d026e8d0101)
+// (sepolia, 0xae9b476d8eed73897b0f71ac59c267856dbae64f249518fea862377208436cc5)
+
+fn chain_id_to_url() -> HashMap<u32, &'static str> {
+    let mut map = HashMap::new();
+    map.insert(0x1, "https://mainnet.infura.io/v3/");
+    map.insert(0x4268, "https://holesky.infura.io/v3/");
+    map.insert(0xaa36a7, "https://sepolia.infura.io/v3/");
+    map
+}
+
+
 #[tokio::main]
 async fn main() -> Result<()> {
+
+    let my_map = chain_id_to_url(); //retrieve the HashMap
+
+    println!("Test chain_id_to_name: 0x1 should be mainnet");
+    if let Some(url_prefix) = my_map.get(&0x1) {
+        println!("The network name for chain ID 0x1 is {}", url_prefix);
+    }
 
     //load .env file
     dotenv().ok();
@@ -26,10 +48,18 @@ async fn main() -> Result<()> {
     //Load infura API key from .env file
     let infura_api_key = std::env::var("INFURA_API_KEY").expect("INFURA_API_KEY must be set");
     //build URL
-    let url = format!("https://mainnet.infura.io/v3/{}", infura_api_key);
-    // Connect to the network via Infura
-    let provider = Provider::<Http>::try_from(url.as_str())?;
+    let url_prefix = my_map.get(&0x1).unwrap();
+    //println!("The network name for chain ID 0x1 is {}", url_prefix);
+    
 
+    let url = url_prefix.to_string() + &infura_api_key.to_string();
+    println!("The URL is {}", url);
+    // Connect to the network via Infura
+    
+    //let url = format!("https://mainnet.infura.io/v3/{}", infura_api_key);
+    //let provider = Provider::<Http>::try_from(url.as_str())?;
+
+    let provider = Provider::<Http>::try_from(url)?;
     //This is an alternative way to connect to the network via Infura with an Public API key
     //let provider = Provider::<Http>::try_from("https://mainnet.infura.io/v3/3d110a0fce9e49b08d2ee584e19a05ba")?;
 
