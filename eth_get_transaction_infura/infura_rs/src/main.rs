a96ab1ab34cac121597ab221777e06ac0d6fc9ee
Tim Bansemer <tim.bansemer@inblock.io>
2024-06-05 22:16:05 +0200

    Add blocktime support and return it

--- eth_get_transaction_infura/infura_rs/src/main.rs
+++ eth_get_transaction_infura/infura_rs/src/main.rs
@@ -1,13 +1,17 @@
+use dotenv::dotenv;
 use ethers::{
     middleware::SignerMiddleware,
+    prelude::*,
     providers::{Http, Middleware, Provider},
     signers::{LocalWallet, Signer},
-    prelude::*
 };
 use eyre::Result;
-use std::convert::TryFrom;
-use dotenv::dotenv;
+use serde::Deserialize;
+use serde_json::json;
+use serde_json::{from_value, Value};
 use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::str::FromStr;
 
 //adding tests
 #[cfg(test)]
@@ -35,8 +39,18 @@ fn chain_id_to_url() -> HashMap<u32, &'static str> {
     map
 }
 
-async fn get_tx_data(chain_id:u32, tx_hash:&str) -> Result<()> {
+#[derive(Deserialize, Debug)]
+struct CustomTransaction {
+    blockNumber: String,
+    input: String, // Add other fields if needed
+}
 
+#[derive(Deserialize, Debug)]
+struct Blocktime {
+    timestamp: String, // Add other fields if needed
+}
+
+async fn get_tx_data(chain_id: u32, tx_hash: &str) -> Result<(H512, u64)> {
     let my_map = chain_id_to_url(); //retrieve the HashMap
 
     if let Some(url_prefix) = my_map.get(&chain_id) {
@@ -50,15 +64,10 @@ async fn get_tx_data(chain_id:u32, tx_hash:&str) -> Result<()> {
     let infura_api_key = std::env::var("INFURA_API_KEY").expect("INFURA_API_KEY must be set");
     //build URL
     let url_prefix = my_map.get(&chain_id).unwrap();
-    //println!("The network name for chain ID 0x1 is {}", url_prefix);
-    
 
     let url = url_prefix.to_string() + &infura_api_key.to_string();
     println!("The URL is {}", url);
     // Connect to the network via Infura
-    
-    //let url = format!("https://mainnet.infura.io/v3/{}", infura_api_key);
-    //let provider = Provider::<Http>::try_from(url.as_str())?;
 
     let provider = Provider::<Http>::try_from(url)?;
     //This is an alternative way to connect to the network via Infura with an Public API key
@@ -67,32 +76,45 @@ async fn get_tx_data(chain_id:u32, tx_hash:&str) -> Result<()> {
     let chain_id = provider.get_chainid().await?;
 
     // Define the signer.
-    // Define the the SIGNER_PRIVATE_KEY with 
-    // the private key of your Ethereum account (without the 0x prefix) in the .env file. 
+    // Define the the SIGNER_PRIVATE_KEY with
+    // the private key of your Ethereum account (without the 0x prefix) in the .env file.
     let wallet_key = std::env::var("SIGNER_PRIVATE_KEY").expect("SIGNER_PRIVATE_KEY must be set");
-    let wallet: LocalWallet = wallet_key.parse::<LocalWallet>()?
-    .with_chain_id(chain_id.as_u64());
+    let wallet: LocalWallet = wallet_key
+        .parse::<LocalWallet>()?
+        .with_chain_id(chain_id.as_u64());
 
     // connect the wallet to the provider
     let client = SignerMiddleware::new(provider, wallet);
 
-    //Ethereum_types {H160,H256,U256} https://kauri.io/#collections/A%20Hackathon%20Survival%20Guide/sending-ethereum-transactions-with-rust/
-   //let transaction_hash: H256 ="0x9d4897d3e381982ee872cb193469d991cce8d087f0cd5fe275926f80c1326a1e".parse().unwrap();
-   let transaction_hash: H256 =tx_hash.parse().unwrap();
+    let transaction_hash: H256 = tx_hash.parse().unwrap();
 
-   let tx = client.get_transaction(transaction_hash).await?;
+    let tx = client.get_transaction(transaction_hash).await.unwrap();
+    let tx_json = json!(tx);
+    let tx: CustomTransaction = from_value(tx_json).unwrap();
+    let hex_str: &str = &tx.blockNumber.to_string();
+    let blocknumber = u64::from_str_radix(&hex_str[2..], 16).unwrap();
 
-   println!("Sent tx: {}\n", serde_json::to_string(&tx)?);
+    let timestamp_res = client.get_block(blocknumber).await?;
+    let blocktime: Blocktime = from_value(json!(timestamp_res)).unwrap();
+    let blocktime_u64 = u64::from_str_radix(&blocktime.timestamp[2..], 16).unwrap();
+    let txhash_str: &str = &tx.input;
+    let input = tx.input[10..].parse::<H512>().unwrap();
 
-   Ok(())
+    Ok((input, blocktime_u64))
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-//mainnet
-//Ok(get_tx_data(0x1,"0x9d4897d3e381982ee872cb193469d991cce8d087f0cd5fe275926f80c1326a1e").await?)
-//holesky
-//Ok(get_tx_data(0x4268,"0xe20ee33fe150423099d6c22bf84683e19d03e40371e2c76e59293d026e8d0101").await?)
-//sepolia
-Ok(get_tx_data(0xaa36a7,"0xae9b476d8eed73897b0f71ac59c267856dbae64f249518fea862377208436cc5").await?)
+    //mainnet
+    //Ok(get_tx_data(0x1,"0x9d4897d3e381982ee872cb193469d991cce8d087f0cd5fe275926f80c1326a1e").await?)
+    //holesky
+    //Ok(get_tx_data(0x4268,"0xe20ee33fe150423099d6c22bf84683e19d03e40371e2c76e59293d026e8d0101").await?)
+    //sepolia
+    //Ok(get_tx_data(0xaa36a7,"0xae9b476d8eed73897b0f71ac59c267856dbae64f249518fea862377208436cc5").await?)
+    let result = get_tx_data(
+        0xaa36a7,
+        "0xd82cb4b91a83124fdd2aa367256c22b94276cbc046d1cf56379035fb13a9dd00",
+    );
+    println!("The result is {:?}", result.await?);
+    Ok(())
 }
